188d02374727182c4c108e4a2443716814d2766b
rkblackfire <krug@tecware-gmbh.de>
2018-01-17 16:22:51 +0100

    dbg: Pushing Contacts with no Name/Whitespace

--- PPSnBaseCfg/cfg/mdm/contacts/Wpf/contacts.lua
+++ PPSnBaseCfg/cfg/mdm/contacts/Wpf/contacts.lua
@@ -30,9 +30,11 @@ pushContact = command(
 		
 pushContact = command(
 	function (args) : void
-		msgbox("Das Feld Name darf nicht leer sein." );
-			return;
 		UpdateSources();
+		if Data:Head:First:Name == nil or (Data:Head:First:Name:gsub("^%s*", "")) == ''  then
+			msgbox("Das Feld Name darf nicht leer sein."  );
+			return;
+		end
 		if Data:IsDirty or Data:Object:IsDocumentChanged then
 			await(PushDataAsync());
 		else
