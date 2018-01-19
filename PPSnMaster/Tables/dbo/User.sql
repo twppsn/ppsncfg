aafa24edbe4a8abfabedf2c2be649fad5c2add70
neolithos <neolithos@outlook.com>
2018-01-19 09:41:53 +0100

    Add: User<->Pers

--- PPSnMaster/Tables/dbo/User.sql
+++ PPSnMaster/Tables/dbo/User.sql
@@ -4,6 +4,7 @@
 	[Login] [sys].[sysname] NULL, 
 	[Security] NVARCHAR(MAX) NULL, 
 	[LoginVersion] BIGINT NOT NULL CONSTRAINT dfUserLoginVersion DEFAULT 0, 
+    [PersId] BIGINT NULL CONSTRAINT fkUserPersId REFERENCES dbo.Pers (Id), 
 )
 GO
 ALTER TABLE [dbo].[User] ENABLE CHANGE_TRACKING;
@@ -35,3 +36,11 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level2type = N'COLUMN',
     @level2name = N'LoginVersion'
 GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'User Pers assignment',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'PersId'
