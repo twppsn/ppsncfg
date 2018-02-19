8cd3b1dc231f5879347af12b1437f90218fb38e0
neolithos <neolithos@outlook.com>
2018-02-19 12:41:34 +0100

    Add: Empty currencies.lua

---
+++ PPSnBaseCfg/cfg/mdm/constants/Wpf/currencies.lua
@@ -0,0 +1,20 @@
+﻿
+-- Global Properties --
+Title = "Einstellung";
+
+-- UI Creation --
+local ctrl = UI.Grid {
+	RowDefinitions = { "*" },
+	ColumnDefinitions = { "*" },
+		
+	UI.Label {
+		Content = "Todo Währungseinstellungen"
+	}
+};
+
+
+setControl {
+	Title = UI.Binding("Title"),
+	SubTitle = "Währungseinstellungen",
+	ctrl
+}
