b2759c0335e2e2846bc46e7853febfecfd515642
Matthias Stein <stein@tecware-gmbh.de>
2021-09-10 16:34:59 +0200

    Chg: Backup parameter

--- PPSnBaseCfg/cfg/System.lua
+++ PPSnBaseCfg/cfg/System.lua
@@ -166,7 +166,7 @@ local function executeBackup(db, checkDb, beforeActions, afterActions) : bool
 					backupCommand.CommandText = [==[
 						BACKUP DATABASE []==] .. databaseName ..  [==[] 
 							TO DISK = N']==] .. backupFile .. [==['
-							WITH NOFORMAT, INIT, NAME = N']==] .. "Vollständige Sicherung vom " .. now:ToString("G") .. [==[', SKIP, NOREWIND, NOUNLOAD, STATS = 10, CHECKSUM
+							WITH NOFORMAT, INIT, NAME = N']==] .. "Vollständige Sicherung vom " .. now:ToString("G") .. [==[', SKIP, NOREWIND, NOUNLOAD, STATS = 10, CHECKSUM, BUFFERCOUNT = 8, MAXTRANSFERSIZE = 4194304, BLOCKSIZE = 4096
 					]==];
 					backupCommand:ExecuteNonQuery();
 
@@ -187,7 +187,7 @@ local function executeBackup(db, checkDb, beforeActions, afterActions) : bool
 					backupCommand.CommandText = [==[
 						BACKUP DATABASE []==] .. databaseName ..  [==[] 
 							TO DISK = N']==] .. backupFile .. [==['
-							WITH DIFFERENTIAL, NOINIT, NAME = N']==] .. "Differential Sicherung vom " .. now:ToString("G") .. [==[', SKIP, NOREWIND, NOUNLOAD, STATS = 10, CHECKSUM
+							WITH DIFFERENTIAL, NOINIT, NAME = N']==] .. "Differential Sicherung vom " .. now:ToString("G") .. [==[', SKIP, NOREWIND, NOUNLOAD, STATS = 10, CHECKSUM, BUFFERCOUNT = 8, MAXTRANSFERSIZE = 4194304, BLOCKSIZE = 4096
 					]==];
 					backupCommand:ExecuteNonQuery();
 
